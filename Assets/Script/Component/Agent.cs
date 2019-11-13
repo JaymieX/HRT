46e3a83e9eae964c4b92ae3f25326c38313db8e2
Jaymie <jaymie.xsj@gmail.com>
2019-11-13 15:06:10 -0500

    Added partical

--- Assets/Script/Component/Agent.cs
+++ Assets/Script/Component/Agent.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class Agent : MonoBehaviour
 {
+    [SerializeField]
+    private string despawnPartical;
+
     internal delegate void CollisionEnter2DHandler(GameObject sender, Collision2D col);
 
     internal event CollisionEnter2DHandler OnCollisionEnter2DEvent;
 
+    private bool _lock = true;
+
     private void OnCollisionEnter2D(Collision2D col)
     {
         OnCollisionEnter2DEvent?.Invoke(gameObject, col);
+    }
+
+    void OnDisable()
+    {
+        if (!_lock)
+        {
+            ObjectPool.Instance.SpawnAt(despawnPartical, transform.position);
+        }
+
+        _lock = false;
     }
 
     private void Update()
---
+++ Assets/Script/Component/Life.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Life : MonoBehaviour
+{
+    [SerializeField]
+    private float time;
+
+    [SerializeField]
+    private string poolID;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        StartCoroutine(Count());
+    }
+
+    private IEnumerator Count()
+    {
+        yield return new WaitForSeconds(time);
+
+        ObjectPool.Instance.Kill(poolID, gameObject);
+    }
+}
